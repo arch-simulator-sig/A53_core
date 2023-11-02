a58c5739900472842c8d4db39c6c4ddc498b5898
dzw1208 <1018462681@qq.com>
2023-11-02 11:06:14 +0800

    Delete : defines.v INST_ADD

--- projects/cpu_diff/vsrc/defines.v
+++ projects/cpu_diff/vsrc/defines.v
@@ -4,7 +4,7 @@
 `define ZERO_WORD  64'h00000000_00000000
 `define PC_START   64'h00000000_80000000  
 `define REG_BUS    63 : 0     
-`define INST_ADD   8'h11
+
 
 `define RISCV_PRIV_MODE_U   0
 `define RISCV_PRIV_MODE_S   1
